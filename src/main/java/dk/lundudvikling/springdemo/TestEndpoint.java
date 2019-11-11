6eb039d3da4ff14e0a2ef7765b69d7485b4e7830
Martin Lund <privatmartinlund@gmail.com>
2019-11-11 20:33:54 +0100

    Added test logging

--- src/main/java/dk/lundudvikling/springdemo/TestEndpoint.java
+++ src/main/java/dk/lundudvikling/springdemo/TestEndpoint.java
@@ -1,16 +1,22 @@
 package dk.lundudvikling.springdemo;
 
 import io.micrometer.core.annotation.Timed;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("test")
 @Timed
 public class TestEndpoint {
+
+    Logger logger = LoggerFactory.getLogger(TestEndpoint.class);
+
 
     @Autowired
     private BasicConfiguration basicConfiguration;
@@ -20,6 +26,11 @@ public class TestEndpoint {
 
     @GetMapping
     public String getTest(){
+        logger.trace("A TRACE Message");
+        logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
         return basicConfiguration.getMessage();
     }
 
