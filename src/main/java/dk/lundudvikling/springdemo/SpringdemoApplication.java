411e24e3e2e118cd4fe457f96dd3d4af72fd61a0
Martin Lund <privatmartinlund@gmail.com>
2020-04-23 20:08:57 +0200

    Added spring boot test to the project

--- src/main/java/dk/lundudvikling/springdemo/SpringdemoApplication.java
+++ src/main/java/dk/lundudvikling/springdemo/SpringdemoApplication.java
@@ -1,16 +1,11 @@
 package dk.lundudvikling.springdemo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Profile;
-
-import javax.sql.DataSource;
 
 @SpringBootApplication
 public class SpringdemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringdemoApplication.class, args);
 	}
-
 }
