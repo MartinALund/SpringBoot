7ae4fa230f9595da9c6090909f16f06698bccdf0
Martin Lund <privatmartinlund@gmail.com>
2020-08-09 11:14:13 +0200

    Added controller unit tests

--- src/test/java/dk/lundudvikling/springdemo/people/implementations/controllers/PersonControllerImplTest.java
+++ src/test/java/dk/lundudvikling/springdemo/people/implementations/controllers/PersonControllerImplTest.java
@@ -12,8 +12,9 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
 
 @RunWith(SpringRunner.class)
 public class PersonControllerImplTest {
@@ -42,6 +43,53 @@ public class PersonControllerImplTest {
     public void should_returnHttpStatus500_whenGetPeopleCallFailsUnexpectedly() throws JpaException {
         doThrow(JpaException.class).when(mockedService).getPeople();
         Assert.assertEquals(500, systemUnderTest.getPeople().getStatusCodeValue());
+    }
+
+    @Test
+    public void should_returnHttpStatus201_whenCreatePersonIsCalled() throws JpaException{
+        when(mockedService.createPerson(any())).thenReturn(fakeDataProvider.getFakePerson());
+        Assert.assertEquals(201, systemUnderTest.createPerson(fakeDataProvider.getFakePerson()).getStatusCodeValue());
+    }
+
+    @Test
+    public void should_returnHttpStatus500_whenCreatePersonFailsUnexpectedly() throws JpaException{
+        doThrow(JpaException.class).when(mockedService).createPerson(any());
+        Assert.assertEquals(500, systemUnderTest.createPerson(fakeDataProvider.getFakePerson()).getStatusCodeValue());
+    }
+
+    @Test
+    public void should_returnHttpStatus200_whenUpdatePersonIsCalled() throws JpaException{
+        when(mockedService.updatePerson(any())).thenReturn(fakeDataProvider.getFakePerson());
+        Assert.assertEquals(200, systemUnderTest.updatePerson(fakeDataProvider.getFakePerson()).getStatusCodeValue());
+    }
+
+    @Test
+    public void should_returnHttpStatus500_whenUpdatePersonFailsUnexpectedly() throws JpaException{
+        doThrow(JpaException.class).when(mockedService).updatePerson(any());
+        Assert.assertEquals(500, systemUnderTest.updatePerson(fakeDataProvider.getFakePerson()).getStatusCodeValue());
+    }
+
+    @Test
+    public void should_returnHttpStatus200_whenGetPersonByIdIsCalled() throws JpaException{
+        when(mockedService.getPerson(anyLong())).thenReturn(fakeDataProvider.getFakePerson());
+        Assert.assertEquals(200, systemUnderTest.getPersonById(123L).getStatusCodeValue());
+    }
+
+    @Test
+    public void should_returnHttpStatus500_whenGetPersonByIdFailsUnexpectedly() throws JpaException{
+        doThrow(JpaException.class).when(mockedService).getPerson(anyLong());
+        Assert.assertEquals(500, systemUnderTest.getPersonById(123L).getStatusCodeValue());
+    }
+
+    @Test
+    public void should_returnHttpStatus204_whenGetDeletePersonByIdIsCalled(){
+        Assert.assertEquals(204, systemUnderTest.deletePersonById(123L).getStatusCodeValue());
+    }
+
+    @Test
+    public void should_returnHttpStatus500_whenDeletePersonByIdFailsUnexpectedly() throws JpaException{
+        doThrow(JpaException.class).when(mockedService).deletePerson(anyLong());
+        Assert.assertEquals(500, systemUnderTest.deletePersonById(123L).getStatusCodeValue());
     }
 
 }
