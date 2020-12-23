3454fc6988391e6421cdec4880651f0a1aa4f80c
Martin Lund <privatmartinlund@gmail.com>
2020-12-23 06:47:20 +0100

    Added aspect

--- src/main/java/dk/lundudvikling/springdemo/SpringdemoApplication.java
+++ src/main/java/dk/lundudvikling/springdemo/SpringdemoApplication.java
@@ -2,8 +2,10 @@ package dk.lundudvikling.springdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class SpringdemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringdemoApplication.class, args);
--- src/main/java/dk/lundudvikling/springdemo/internalization/InternalizationController.java
+++ src/main/java/dk/lundudvikling/springdemo/internalization/InternalizationController.java
@@ -1,12 +1,10 @@
 package dk.lundudvikling.springdemo.internalization;
 
+import dk.lundudvikling.springdemo.aspects.Loggable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("internalization")
@@ -20,6 +18,8 @@ public class InternalizationController {
     }
 
     @GetMapping("{languageCode}/{text}")
+
+    @Loggable
     public ResponseEntity<String> getInternalizedText(@PathVariable String languageCode, @PathVariable String text){
         return new ResponseEntity<>(messageTranslator.getInternalizedString(text, languageCode), HttpStatus.OK);
     }
